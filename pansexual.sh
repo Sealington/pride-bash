6448cc9749b98c80d18140538693a03ae8cd29f9
Sealington <150572349+Sealington@users.noreply.github.com>
2026-04-15 08:50:55 -0400

    file

--- pansexual.sh
+++ pansexual.sh
@@ -4,7 +4,6 @@ function pansexual() {
   local p="\e[38;5;198m"
   local y="\e[38;5;220m"
   local b="\e[38;5;39m"
-  #if [ -z "$1" ];
   local c="█"
   local d=${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}
   echo -e ${p}${d}
