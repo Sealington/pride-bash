a4555e3cfd60fe8f8712443700dcf4f094b31c69
Sealington <150572349+Sealington@users.noreply.github.com>
2026-04-15 08:49:36 -0400

    file

--- bisexual.sh
+++ bisexual.sh
@@ -4,7 +4,6 @@ function bisexual() {
   local m="\e[38;5;161m"
   local p="\e[38;5;133m"
   local b="\e[38;5;20m"
-  #if [ -z "$1" ];
   local c="█"
   local d=${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}
   echo -e ${m}${d}
