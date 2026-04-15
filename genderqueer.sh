84ae20efd4f35f2c12434c799d45ad61dfa6ebf4
Sealington <150572349+Sealington@users.noreply.github.com>
2026-04-15 08:50:15 -0400

    file

--- genderqueer.sh
+++ genderqueer.sh
@@ -4,7 +4,6 @@ function genderqueer() {
   local p="\e[38;5;140m"
   local w="\e[38;5;15m"
   local g="\e[38;5;64m"
-  #if [ -z "$1" ];
   local c="█"
   local d=${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}
   echo -e ${p}${d}
