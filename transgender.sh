181df2e45a2a3eadd8ca1af40854b38bb450e4f6
Sealington <150572349+Sealington@users.noreply.github.com>
2026-04-15 08:51:21 -0400

    file

--- transgender.sh
+++ transgender.sh
@@ -4,7 +4,6 @@ function transgender() {
   local b="\e[38;5;81m"
   local p="\e[38;5;218m"
   local w="\e[38;5;15m"
-  #if [ -z "$1" ];
   local c="█"
   local d=${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}
   echo -e ${b}${d}
